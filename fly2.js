c12e8a1af909bcb16582cd44294d875872bff0a0
bengarfield <ben-garfield@hotmail.co.uk>
2017-03-22 06:58:09 +0000

    fly

--- fly2.js
+++ fly2.js
@@ -1,79 +1,120 @@
+altspace.getThreeJSTrackingSkeleton().then(function(skeleton){
+  sim.scene.add(skeleton)
+  var spine = skeleton.getJoint('Spine');
+  var handPos;
+  var gripPos = new THREE.Vector3();
+  var spinePos = new THREE.Vector3(spine.position.x, spine.position.y, spine.position.z);
+  console.log(spine);
+  altspace.getGamepads();
+  var pads =[];
+  var box = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshBasicMaterial());
+  box.position.y = 1;
+  //sim.scene.add(box);
+  new NativeComponent('n-box-collider', {type: 'hologram',size: '1 1 1'}, box).addTo(sim.scene);
+  box.position.x = 1;
+  box.addEventListener('cursorup', function(){
+	console.log('click');
+	pads = altspace.getGamepads();
+	console.log(pads[2].pose.position);
+  });
+  var fly = new THREE.Group();
+  var walls = new THREE.Group();
+  fly.add(walls);
+  var mat = new THREE.MeshBasicMaterial();
+  mat.visible = false;
+  console.log(mat);
+  var floor = new THREE.Mesh(new THREE.PlaneGeometry(1000,1000), mat);
+  floor.rotation.x = -Math.PI/2;
+  new NativeComponent('n-mesh-collider', {type: 'environment', convex: 'false'}, floor).addTo(fly);
+  var wall1 = new THREE.Mesh(new THREE.PlaneGeometry(1.5,2), mat);
+  wall1.position.set(0,1,.5);
+  new NativeComponent('n-mesh-collider', {type: 'environment', convex: 'false'}, wall1).addTo(walls);
+  var wall2 = wall1.clone();
+  wall2.position.set(0,1,-.5);
+  wall1.rotation.y = Math.PI;
+  var wall3 = wall1.clone();
+  var wall4 = wall1.clone();
+  wall3.position.set(.5,1,0);
+  wall4.position.set(-.5,1,0);
+  wall3.rotation.y = -Math.PI/2;
+  wall4.rotation.y = Math.PI/2;
+  new NativeComponent('n-mesh-collider', {type: 'environment', convex: 'false'}, wall2).addTo(walls);
+  new NativeComponent('n-mesh-collider', {type: 'environment', convex: 'false'}, wall3).addTo(walls);
+  new NativeComponent('n-mesh-collider', {type: 'environment', convex: 'false'}, wall4).addTo(walls);
+  sim.scene.add(fly);
+  
+  var sphere = new THREE.Mesh(new THREE.SphereGeometry(.01), new THREE.MeshBasicMaterial());
+  new NativeComponent('n-cockpit-parent', null, sphere).addTo(sim.scene);
+  //sim.scene.add(sphere);
+  var button = new THREE.Mesh(new THREE.BoxGeometry(.2,.2,.05), new THREE.MeshBasicMaterial());
+  button.position.set(-.8,.3,-1.5);
+  console.log(button);
+  new NativeComponent('n-box-collider', {type: 'hologram',size: '.3 .3 .1'}, button);
+  new NativeComponent('n-cockpit-parent', null, button).addTo(sim.scene);
+  button.userData.altspace.collider.enabled = true;
+  button.addEventListener('cursorup', function(){
+	pads = altspace.getGamepads();
+	console.log(pads[2]);
+	if(pads[2]){
+	  grip()
+	}
+  });
+  
+  var textGroup = new THREE.Group();
+  var config = { text: 'hello',fontSize: 20, horizontalAlign: 'middle' };
+  var text = new NativeComponent('n-text', config).addTo(button);
+  text.object.position.y = 1;
+  console.log(text);
+  new NativeComponent('n-cockpit-parent', null, textGroup).addTo(sim.scene);
 
-	altspace.getGamepads();
-	altspace.getThreeJSTrackingSkeleton().then(function(skeleton){
-		headTrack = skeleton.getJoint("Eye");
+  var gripped = false;
 
-		var plane1 = new THREE.Mesh(new THREE.PlaneGeometry(10, 10));
-		plane1.rotation.x = THREE.Math.degToRad(-90);
-		plane1.position.y = 0.3;
-		var collider = new NativeComponent('n-mesh-collider', {type: "environment", convex: false}, plane1);
-		
-		var plane2 = new THREE.Mesh(new THREE.PlaneGeometry(1, 1)); 
-		plane2.position.set(0, 2, 0);
-		plane2.addEventListener("cursorup", function(){getContollers()});
-		
-		var plane3 = new THREE.Mesh(new THREE.PlaneGeometry(11, 11));
-		plane3.rotation.x = THREE.Math.degToRad(-90);
-		plane3.position.y = -1.3;
-		var collider = new NativeComponent('n-mesh-collider', {type: "environment", convex: false}, plane3);
-		
-		var plane4 = new THREE.Mesh(new THREE.PlaneGeometry(12, 12));
-		plane4.rotation.x = THREE.Math.degToRad(-90);
-		plane4.position.y = -2.3;
-		var collider = new NativeComponent('n-mesh-collider', {type: "environment", convex: false}, plane4);
-		
-		var plane5 = new THREE.Mesh(new THREE.PlaneGeometry(13, 13));
-		plane5.rotation.x = THREE.Math.degToRad(-90);
-		plane5.position.y = -3.3;
-		var collider = new NativeComponent('n-mesh-collider', {type: "environment", convex: false}, plane5);
-		sim.scene.add(plane1, plane2, plane3, plane4, plane5);
-		
-		function getContollers(){
-			gamepads = altspace.getGamepads();
-			plane1.material.visible = 0;
-			plane1.scale.setScalar(100);
-			sim.scene.remove(plane2, plane3, plane4, plane5);
-			heightOffset = headTrack.position.y - .3;
-			gripState();
-			gripAction1();
-		};
-		
-		function gripState(){
-			if(gamepads[2].buttons[1].value > .5)gp1Grip = true;
-			else gp1Grip = false;
-			if(gamepads[2].buttons[2].value == 1)plane1.position.y = -100;
-			requestAnimationFrame(gripState);
-		};
-		
-		var inLoop1 = false;
-		
-		function gripAction1(){
-			var planePos = new THREE.Vector3;
-			var freeze = new THREE.Vector3;
-			var offset = new THREE.Vector3;
-			
-			if(gp1Grip && !inLoop1){
-				freeze.subVectors(headTrack.position, gamepads[2].position)
-				loop();
-			};
-			//if(!gp1Grip )plane1.position.y = -100;
-			
-			function loop(){
-				inLoop1 = true;
-				if(!gp1Grip){
-					inLoop1 = false;
-					return;
-				}
-				planePos.copy(headTrack.position);
-				planePos.y -= heightOffset;
-				planePos.add(freeze);
-				offset.subVectors(headTrack.position, gamepads[2].position);
-				planePos.sub(offset);
-				plane1.position.copy(planePos);
+  function grip() {
+	gripVal1 = pads[1].buttons[1].value;
+	gripVal2 = pads[2].buttons[1].value;
+	button.material.color.set('rgb(' + Math.round(gripVal2 * 255) + ',0,' + Math.round(gripVal1 * 255) + ')');
+	handPos = new THREE.Vector3(pads[2].position.x, pads[2].position.y, pads[2].position.z);
+	spinePos = new THREE.Vector3(spine.position.x, spine.position.y, spine.position.z);
+	spherePos = handPos.sub(spinePos);
+	if (gripVal2 > .5 && !gripped) {
+	  gripped = true;
+	  //sphere.visible = true;
+	  console.log('grip');
+	  gripPos.copy(handPos);
+	  sphere.position.set(handPos.x, handPos.y + .5, handPos.z);
+	  fly.position.set(spine.position.x, spine.position.y -1, spine.position.z)
+	  console.log(sphere.position);
+	  walls.position.y = 0;
+	} else if (gripVal2 < .5 && gripped) {
+	  gripped = false;
+	  sphere.visible = false;
+	  walls.position.y = -2;
+	  console.log('release');
+	}
+	//console.log(handPos.sub(gripPos));
+	if (gripped){
+	  var move = handPos.sub(gripPos);
+	  fly.position.add(move.divideScalar(2));
+	}
+	if (pads[2].buttons[2].value == 1) {
+	  fly.position.y = -10;
+	}
+	config.text = Math.round(spine.position.y - 1);
+	text.update(config);
+	text.object.position.y = spine.position.y;
+	//sphere.position.set(handPos.x, handPos.y, handPos.z);
+	requestAnimationFrame(grip);
+  }
 
-				requestAnimationFrame(loop);
-			};
-			requestAnimationFrame(gripAction1);
-		};
-	});
-
+  console.log(button);
+  function loop() {
+	if(pads[1]) {
+	  //console.log(pads[2].pose.position, pads[2].position);
+	} else {
+	  //console.log('no')
+	}
+	requestAnimationFrame(loop);
+  }
+  loop();
+  //sim.scene.add(button);
+});
---
+++ flyEvil.js
@@ -0,0 +1,79 @@
+
+	altspace.getGamepads();
+	altspace.getThreeJSTrackingSkeleton().then(function(skeleton){
+		headTrack = skeleton.getJoint("Eye");
+
+		var plane1 = new THREE.Mesh(new THREE.PlaneGeometry(10, 10));
+		plane1.rotation.x = THREE.Math.degToRad(-90);
+		plane1.position.y = 0.3;
+		var collider = new NativeComponent('n-mesh-collider', {type: "environment", convex: false}, plane1);
+		
+		var plane2 = new THREE.Mesh(new THREE.PlaneGeometry(1, 1)); 
+		plane2.position.set(0, 2, 0);
+		plane2.addEventListener("cursorup", function(){getContollers()});
+		
+		var plane3 = new THREE.Mesh(new THREE.PlaneGeometry(11, 11));
+		plane3.rotation.x = THREE.Math.degToRad(-90);
+		plane3.position.y = -1.3;
+		var collider = new NativeComponent('n-mesh-collider', {type: "environment", convex: false}, plane3);
+		
+		var plane4 = new THREE.Mesh(new THREE.PlaneGeometry(12, 12));
+		plane4.rotation.x = THREE.Math.degToRad(-90);
+		plane4.position.y = -2.3;
+		var collider = new NativeComponent('n-mesh-collider', {type: "environment", convex: false}, plane4);
+		
+		var plane5 = new THREE.Mesh(new THREE.PlaneGeometry(13, 13));
+		plane5.rotation.x = THREE.Math.degToRad(-90);
+		plane5.position.y = -3.3;
+		var collider = new NativeComponent('n-mesh-collider', {type: "environment", convex: false}, plane5);
+		sim.scene.add(plane1, plane2, plane3, plane4, plane5);
+		
+		function getContollers(){
+			gamepads = altspace.getGamepads();
+			plane1.material.visible = 0;
+			plane1.scale.setScalar(100);
+			sim.scene.remove(plane2, plane3, plane4, plane5);
+			heightOffset = headTrack.position.y - .3;
+			gripState();
+			gripAction1();
+		};
+		
+		function gripState(){
+			if(gamepads[2].buttons[1].value > .5)gp1Grip = true;
+			else gp1Grip = false;
+			if(gamepads[2].buttons[2].value == 1)plane1.position.y = -100;
+			requestAnimationFrame(gripState);
+		};
+		
+		var inLoop1 = false;
+		
+		function gripAction1(){
+			var planePos = new THREE.Vector3;
+			var freeze = new THREE.Vector3;
+			var offset = new THREE.Vector3;
+			
+			if(gp1Grip && !inLoop1){
+				freeze.subVectors(headTrack.position, gamepads[2].position)
+				loop();
+			};
+			//if(!gp1Grip )plane1.position.y = -100;
+			
+			function loop(){
+				inLoop1 = true;
+				if(!gp1Grip){
+					inLoop1 = false;
+					return;
+				}
+				planePos.copy(headTrack.position);
+				planePos.y -= heightOffset;
+				planePos.add(freeze);
+				offset.subVectors(headTrack.position, gamepads[2].position);
+				planePos.sub(offset);
+				plane1.position.copy(planePos);
+
+				requestAnimationFrame(loop);
+			};
+			requestAnimationFrame(gripAction1);
+		};
+	});
+
